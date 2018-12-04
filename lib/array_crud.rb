739cb479ea9d5709a6826afcb9f3feba87bf9630
Upasna Langar <langarupasna.2@gmail.com>
2018-12-04 20:14:19 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,31 +3,34 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  [1,2,3,4]
 end
 
-def add_element_to_end_of_array
+def add_element_to_end_of_array(array=[1,2,3,4,5], element=5)
+array.push(element)
+end
+
+def add_element_to_start_of_array(array=[2,3,4,5,6], element=1)
+  array.unshift(element)
+end
+
+def remove_element_from_end_of_array(array=[1,2,3,4,5,6])
+  array.pop
+end
+
+def remove_element_from_start_of_array(array=[0,1,2,3,4,5,6])
+  array.shift
+end
+
+def retrieve_element_from_index(array=[1,2,3,4,5,6,7], index_number=3)
+  array[index_number]
+  
+end
+
+def retrieve_first_element_from_array(array=[1,2,3,4,5])
+  array[0]
   end
 
-def add_element_to_start_of_array(array, element)
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array[])
-  
-end
+def retrieve_last_element_from_array(array=[1,2,3,4,5])
+  array[-1]
+    end
